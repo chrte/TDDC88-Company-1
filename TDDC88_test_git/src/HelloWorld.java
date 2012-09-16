587d7d455cf137ab8c9d3a22a723e7e8d978691c
chrte707 <chrte707@student.liu.se>
2012-09-16 13:10:38 +0200

    testin more with eclipse

--- TDDC88_test_git/src/HelloWorld.java
+++ TDDC88_test_git/src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World! (maint2)");
-		System.out.println("hello z-app");
+		System.out.println("hello z-app!!");
 	}
 
 }
