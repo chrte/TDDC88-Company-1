e6540189621b7eeeb977a8e7f1c290e42aa789b2
chrte707 <chrte707@student.liu.se>
2012-09-17 18:03:25 +0200

    Added a action to the button, don't do much.

--- TestAndroid/src/com/example/testandroid/MainActivity.java
+++ TestAndroid/src/com/example/testandroid/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.testandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -16,5 +17,9 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        setContentView(R.layout.activity_main);
     }
 }
