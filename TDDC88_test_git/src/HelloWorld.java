990c492edf41add95303013b5a0c29e83491fff8
sebra729 <sebra729@student.liu.se>
2012-09-28 14:07:06 +0200

    Push & branch test

--- TDDC88_test_git/src/HelloWorld.java
+++ TDDC88_test_git/src/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	/**
 	 * @param args
 	 */
+	//Push & branch test /Sebastian
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World! (maint2)");
 		System.out.println("hello z-app!!");
